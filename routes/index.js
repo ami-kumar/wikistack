01e4822cf60ca0bb5fa9e8b3d30b70756538fc71
ami-kumar <ak3284@columbia.edu>
2015-02-23 22:22:18 -0500

    Add a page template

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/addpage', function(req, res, next) {
+	res.render('addpage', { title: 'ADD A PAGE' })
+})
+
 module.exports = router;
